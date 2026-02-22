66c958df1541c81e9131f90fb904f3c5337ec268
Michael Leydon <mwleydon@gmail.com>
2026-02-22 12:55:53 -0500

    Match diagram typography to manuscript body scale

--- submission/render_manuscript_diagrams.py
+++ submission/render_manuscript_diagrams.py
@@ -29,16 +29,16 @@ BASE_GRAPH = (
 BASE_NODE = (
     f'shape="box", style="filled", fillcolor="{NODE_FILL}", '
     f'color="{LINE}", penwidth="1.5", fontname="Times-Roman", '
-    'fontsize="8", margin="0.08,0.06"'
+    'fontsize="13", margin="0.08,0.06"'
 )
 BASE_DECISION = (
     f'shape="diamond", style="filled", fillcolor="{NODE_FILL}", '
-    f'color="{LINE}", penwidth="1.5", fontname="Times-Roman", fontsize="8", '
+    f'color="{LINE}", penwidth="1.5", fontname="Times-Roman", fontsize="13", '
     'margin="0.04,0.04"'
 )
 BASE_EDGE = (
     f'color="{LINE}", penwidth="1.3", arrowsize="0.70", '
-    f'fontname="Times-Roman", fontsize="8", fontcolor="{INK}"'
+    f'fontname="Times-Roman", fontsize="11", fontcolor="{INK}"'
 )
 
 
